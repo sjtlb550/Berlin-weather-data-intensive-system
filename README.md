c7472e31f2628dd46a0b211cafb2f217775fa078
Salah Taleb <56842580+sjtaleb@users.noreply.github.com>
2025-08-15 16:18:04 +0200

    designed and build cassandra database, started spark

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Design and implement a data intensive system to visualize berlin weather 
+
+## Services
+This application is designed to ingest streaming Berlin-weather data, preprocess them, transform them and eventually visualize them.
+all using containerized microservices to ensure reliability, scalability, maintainability and above that, security and governance.
+The system is built using 6 services:
+   NAME            Docker-image                Role
+1. Zookeeper       wurstmeister/zookeeper      kafka management 
+2. Kafka           wurstmeister/kafka          Real-time data streaming
+3. Producer        manually built              Data producing
+4. Spark-app       manually built              Data Preprocessing and sending
+5. cassandra       cassandra                   Database
+6. grafana         grafana/grafana-oss         Visualization
+
+All images are pulled from the Docker-Hub except for the producer and spark-app. The producer is responsible for fetching data streams 
+from https://open-meteo.com/, and sending them to the weather-topic topic in kafka. The spark-app on the other side, is the only consumer,
+and its job is first to read these streams and transform them using spark streming, store them in cassandra to be ultimately visualized using
+grfana.
+
+
+
+## Using Grafana
+Once you lunch the app using docker-compose up, you can access the intended visualizations using grafana.
+1. To do this, go to http://localhost:3000/ to access the GUI on your system.
+2. you will be asked for the user and password: Enter user:Admin, password: Admin
+3. When the home page open, click on "add your first data scource" to add the required plugin.
+4. search for and add hadesarchitect-cassandra-datasource.
+5. To connect,
---
+++ consumer/streaming.py
@@ -0,0 +1,56 @@
+#imports
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StringType, FloatType
+from pyspark.sql.functions import from_json, col, avg, min, max, window,variance, to_timestamp
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+import time
+
+# creating cassandra keyspace,cluster, session, and table
+def cassandra_schema():
+    authentication = PlainTextAuthProvider(username = "cassandra", password = "cassandra")
+    cluster = Cluster(['cassandra'], auth_provider = authentication)
+    session = cluster.connect()
+    session.execute(f"""
+      CREATE KEYSPACE IF NOT EXISTS weather_ks
+      WITH REPLICATION = {{'class':'SimpleStrategy','replication_factor':1}};
+    """)
+    session.set_keyspace("weather_ks")
+    session.execute("""
+        CREATE TABLE IF NOT EXISTS aggregates(
+        metric text,
+        window_start timestamp,
+        window_end timestamp,
+        avg_val float,
+        min_val float,
+        max_val float,
+        var_val float,
+        PRIMARY KEY ((metric), window_start, window_end)
+        );
+        """
+    )
+    session.shutdown()
+    cluster.shutdown()
+
+cassandra_schema()
+time.sleep(2)
+
+
+#Starts Spark / Configuring Spark to connect to Cassandra inside Docker.
+spark = SparkSession.builder\
+    .appName('KafkaToCassandra')\
+    .config("spark.cassandra.connection.host", "cassandra")\
+    .config("spark.cassandra.auth.username", "cassandra")\
+    .config("spark.cassandra.auth.password", "cassandra")\
+    .getOrCreate()
+
+
+#Read from kafka (Connects to Kafka at kafka:9092, subscribes to topic weather-topic)
+raw= spark.readStream \
+    .format('kafka')\
+    .option('kafka.bootstrap.servers', "kafka:9092")\
+    .option("subscribe", "weather-topic")\
+    .load()
+
+
+
